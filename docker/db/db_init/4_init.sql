b6415c9ac8d0d987c215d1dfbd0da467d948f039
imamiya-masaki <imachan567@gmail.com>
2020-09-14 17:58:51 +0900

    dummy wip

---
+++ docker/db/db_init/4_init.sql
@@ -0,0 +1,8 @@
+--dummy
+
+/* */
+insert into book_db.users (id, username, twitter_token) values 
+("A木　太郎", "dwwadamlkdfmwaklrakmralrma"), ("B木　太郎", "dwwadamlkdfmwaklrakmralrma"), ("C木　太郎", "dwwadamlkdfmwaklrakmralrma"), ("D木　太郎", "dwwadamlkdfmwaklrakmralrma"), ("E木　太郎", "dwwadamlkdfmwaklrakmralrma");
+
+insert into book_db.
+
