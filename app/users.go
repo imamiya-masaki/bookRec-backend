68a042c985cc40204e85ec6b9ae9a28d94bb498c
ExtEOi <e.of.i.120@gmail.com>
2020-09-15 11:30:29 +0900

    usersAPI分離

---
+++ app/users.go
@@ -0,0 +1,18 @@
+package main
+
+import (
+	"app/database"
+	"app/models"
+	"strconv"
+	"github.com/gin-gonic/gin"
+)
+
+func apiGetUserdata(c *gin.Context) {
+	println("getting userdata")
+	param := c.Param("id")
+	var id int
+	id, _ = strconv.Atoi(param)
+	data := models.GetUserDataById(database.GetDB(), id)
+	enc, _ := json.Marshal(data)
+	c.JSON(200, string(enc))
+}
