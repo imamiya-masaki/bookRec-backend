ef5d139f94d7e4d37295847bffcd5072f425091b
shin04 <daikon0413@gmail.com>
2020-09-16 16:51:29 +0900

    stack

--- app/handler/dashboardHandler.go
+++ app/handler/dashboardHandler.go
@@ -1,19 +1,35 @@
 package handler
 
+import "app/database"
+
 type ReactionInfo struct {
 	Username      string
 	ReactionImage string
 }
 
-// type DashBoardInfo struct {
-// 	Reactions        []ReactionInfo
-// 	RecommendCount   int
-// 	RecommendedCount int
-// 	ReactionCount    int
-// 	CouponCount      int
-// 	BookCount        int
-// }
+type DashBoardInfo struct {
+	Reactions        []ReactionInfo
+	RecommendCount   int
+	RecommendedCount int
+	ReactionCount    int
+	CouponCount      int
+	BookCount        int
+}
 
-// func getReactionInfo(id) {
+func getReactionInfo(id) {
+	// id : 自分のID
+	recommends := models.GetMyRecommend(database.GetDB(), id)
 
-// }
+	var reaction_infos []ReactionInfo
+	for _, recommend := range recommends {
+		user_id := recommend.ReceiverId
+		reaction_id := recommend.ReactionContentId
+
+		username := models.GetUserDataById(database.GetDB, user_id).Username
+
+		reaction_info := ReactionInfo{
+			Username: username, 
+			TwitterToken: req.TwitterToken
+		}
+	}
+}
