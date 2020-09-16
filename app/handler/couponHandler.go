38696d09d1a3cfcb1b95975a59fb7b7a1a3b1b6e
shin04 <daikon0413@gmail.com>
2020-09-16 15:37:24 +0900

    coupon

---
+++ app/handler/couponHandler.go
@@ -0,0 +1,38 @@
+package handler
+
+import (
+	"strconv"
+
+	"github.com/gin-gonic/gin"
+
+	"app/database"
+	"app/models"
+)
+
+func PostCoupon(c *gin.Context) {
+	coupon := &models.Coupon{}
+	err := c.BindJSON(coupon)
+	if err != nil {
+		println(err)
+	}
+
+	coupon.PostCoupon(database.GetDB())
+
+	c.JSON(200, coupon)
+}
+
+func GetCoupon(c *gin.Context) {
+	param := c.Param("id")
+	id, _ := strconv.Atoi(param)
+	coupon := models.GetBook(database.GetDB(), id)
+
+	c.JSON(200, coupon)
+}
+
+func GetCouponByUser(c *gin.Context) {
+	param := c.Query("user_id")
+	id, _ := strconv.Atoi(param)
+	coupons := models.GetCouponByUser(database.GetDB(), id)
+
+	c.JSON(200, coupons)
+}
