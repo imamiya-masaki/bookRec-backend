a57ea1ded20115561eb3594b6383155d170f8cd8
shin04 <daikon0413@gmail.com>
2020-09-16 14:37:36 +0900

    books修正

--- docker/db/db_init/3_init.sql
+++ docker/db/db_init/3_init.sql
@@ -42,7 +42,8 @@ CREATE TABLE books (
     title VARCHAR(64),
     author VARCHAR(64),
     price INT,
-    release_date VARCHAR(128),
+    release_date TIMESTAMP,
+    uri VARCHAR(256)
 );
 
 CREATE TABLE book_contents (
