9900ac152c5249a5e3720bac502337c70f6a501b
ExtEOi <e.of.i.120@gmail.com>
2020-09-17 16:23:20 +0900

    ダミーデータ編集

--- docker/db/db_init/4_init.sql
+++ docker/db/db_init/4_init.sql
@@ -1,16 +1,16 @@
 /* */
 INSERT INTO book_db.users (username, twitter_token) VALUES 
-("A木　太郎", "dwwadamlkdfmwaklrakmralrma"), ("B木　太郎", "dwwadamlkdfmwaklrakmralrma"), ("C木　太郎", "dwwadamlkdfmwaklrakmralrma"), ("D木　太郎", "dwwadamlkdfmwaklrakmralrma"), ("E木　太郎", "dwwadamlkdfmwaklrakmralrma");
+("A木　太郎", "dwwadamlkdfmwaklrakmralrma"), ("B木　太郎", "twitterid2"), ("C木　太郎", "twitterid3"), ("D木　太郎", "twitterid4"), ("E木　太郎", "twitterid5");
 INSERT INTO book_db.recommends (sender_id, receiver_id, book_id, reaction_content_id) VALUES
-(1,2, 1, 1),(1,3,1,1),(2,3,2,1);
+(1,2, 1, 0),(1,3,1,0),(2,3,2,0),(3,4,2,0),(4,5,3,0),(5,1,3,0);
 INSERT INTO book_db.messages (reccomend_id, sendedtime, content) VALUES
 (1,NULL,"dada"),(2,NULL,"dada"),(3,NULL,"dada");
 INSERT INTO book_db.book_group (bookname) VALUES
-("毀滅のヤイバ!!"),("なると");
+("きめつ"),("なると");
 INSERT INTO book_db.books (book_group_id, title, author, price) VALUES
-(1,"毀滅のヤイバ！！(1)","吾峠呼世晴",3000),(1,"毀滅のヤイバ！！(2)","吾峠呼世晴",3000),(2,"なると(10)","岸本 斉史",3000);
+(1,"きめつ(1)","吾峠呼世晴",1000),(1,"きめつ(2)","吾峠呼世晴",1000),(2,"なると(10)","岸本 斉史",3000);
 INSERT INTO book_db.my_books (user_id, book_id) VALUES
-(1,1),(2,1),(1,2),(1,3);
+(1,1),(1,2),(1,3),(2,1),(2,2),(3,3);
 INSERT INTO book_db.booklist (user_id, book_id) VALUES
 (1,1),(1,2),(1,3);
 INSERT INTO book_db.reaction_contents (reaction_name, uri) VALUES
