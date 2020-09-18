af79870c90e59a70939e8b25c45439267f5613c2
shin04 <daikon0413@gmail.com>
2020-09-18 10:05:41 +0900

    sql更新

--- docker/db/db_init/4_init.sql
+++ docker/db/db_init/4_init.sql
@@ -7,8 +7,10 @@ INSERT INTO book_db.messages (reccomend_id, sendedtime, content) VALUES
 (1,NULL,"dada"),(2,NULL,"dada"),(3,NULL,"dada");
 INSERT INTO book_db.book_group (bookname) VALUES
 ("きめつ"),("なると");
-INSERT INTO book_db.books (book_group_id, title, author, price) VALUES
-(1,"きめつ(1)","吾峠呼世晴",1000),(1,"きめつ(2)","吾峠呼世晴",1000),(2,"なると(10)","岸本 斉史",3000);
+INSERT INTO book_db.books (book_group_id, title, author, price, uri) VALUES
+(1,"きめつ(1)","吾峠呼世晴",1000,"https://res.cloudinary.com/teamb/image/upload/v1600313784/bj01_pages-to-jpg-0002_vd5k8r.jpg"),
+(1,"きめつ(2)","吾峠呼世晴",1000, "https://res.cloudinary.com/teamb/image/upload/v1600390594/%E3%83%95%E3%82%99%E3%83%A9%E3%83%83%E3%82%AF%E3%82%B7%E3%82%99%E3%82%A7%E3%83%83%E3%82%AF_ehjhvm.jpg"),
+(2,"なると(10)","岸本 斉史",3000, "https://res.cloudinary.com/teamb/image/upload/v1600390603/%E3%83%95%E3%82%99%E3%83%A9%E3%83%83%E3%82%AF%E3%82%B7%E3%82%99%E3%83%A3%E3%83%83%E3%82%AF%E3%81%AB%E3%82%88%E3%82%8D%E3%81%97%E3%81%8F3_mtepjc.jpg");
 INSERT INTO book_db.my_books (user_id, book_id) VALUES
 (1,1),(1,2),(1,3),(2,1),(2,2),(3,3);
 INSERT INTO book_db.booklist (user_id, book_id) VALUES
@@ -17,23 +19,4 @@ INSERT INTO book_db.reaction_contents (reaction_name, uri) VALUES
 (NULL,"hogehoge");
 
 INSERT INTO book_db.coupons (user_id, percent) VALUES (1, 10);
-
-/*
-CREATE TABLE booklist (
-    id INT auto_increment not null primary key,
-    user_id INT,
-    book_id INT
-);
-CREATE TABLE bookContents (
-    id INT auto_increment not null primary key,
-    book_id INT,
-    book_page INT,
-    uri VARCHAR(256)
-);
-CREATE TABLE reactionContent (
-    id INT auto_increment not null primary key,
-    reaction_name VARCHAR(64),
-    uri VARCHAR(256)
-);
-*/
 
